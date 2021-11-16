89aedbd82006d36926dba7db2d7f8c5f211daff2
taixingbi <bitaihang@gmail.com>
2021-11-16 11:37:01 -0500

    add

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -4,8 +4,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("----------app start----------");
 
-        System.out.println("Start");
-        FTPUploader ftpUploader = new FTPUploader("ftp.journaldev.com", "ftpUser", "ftpPassword");
+        String host = "ftp.journaldev.com";
+        String user = "ftpUser";
+        String pwd = "ftpPassword";
+
+        FTPUploader ftpUploader = new FTPUploader(host, user, pwd);
         ftpUploader.uploadFile("1.png", "1.png", "/wonderfl/");
 //        ftpUploader.uploadFile("D:\\Pankaj\\images\\MyImage.png", "1.png", "/wp-content/uploads/image2/");
         ftpUploader.disconnect();
